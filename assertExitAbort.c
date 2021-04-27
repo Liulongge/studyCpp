86ec50b445a047459e8b0d61a753b5b9ae769943
LiuLongge <792286891@qq.com>
2021-04-28 00:45:27 +0800

    NEW: add assert

---
+++ assertExitAbort.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main() {
+   int x = 10;
+   printf("The value of x : %d\n", x);
+   //exit(-1);
+   //abort();
+   //assert(x!=15);
+   return -1;
+   printf("Calling of exit()");
+   return 0;
+}
