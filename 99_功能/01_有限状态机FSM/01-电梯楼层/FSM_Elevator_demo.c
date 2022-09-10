a85b3c3619486b00ff3a7c8f5ddc74ecb9c9cd8f
RungeLiu <792286891@qq.com>
2022-09-10 18:35:26 +0800

    NEW: 添加有限状态机c语言demo

---
+++ 99_功能/01_有限状态机FSM/01-电梯楼层/FSM_Elevator_demo.c
@@ -0,0 +1,160 @@
+/* standard lib */
+#include <stdio.h>
+#include "stdlib.h"
+#include "math.h"
+
+int target_floor_number = 8;
+int current_floor_number = 1;
+int accumulated_floor_number = 0;
+#define TOP_FLOOR 9
+#define BOTTOM_FLOOR 1
+
+/* event array and enum below must be in sync! */
+typedef enum
+{ 
+    _idle = 0, _goingUp = 1, _goingDown = 2, _AtTop = 3, _AtBottom = 4,  _malfunction = 5, _unexpected = 6, _end = 7 
+} state_codes;
+
+typedef enum
+{
+    up = 0, down=1, halt = 2, top = 3, bottom = 4, fail = 5, quit = 6 
+} ret_codes;
+
+int event_idle(void)
+{
+    printf("Idle!\n");
+    printf("Input target floor number in [1, 9] (which floor you want to go?): \n");
+    scanf("%d", &target_floor_number);
+
+    if(current_floor_number < target_floor_number)
+        return up;
+    else if(current_floor_number > target_floor_number)
+        return down;
+    else if(current_floor_number == target_floor_number)
+        return halt;
+    else
+        return fail;
+}
+
+int event_goingUp(void)
+{
+    current_floor_number += 1;
+    accumulated_floor_number += 1;
+    printf("Going up! Floor number is %d\n", current_floor_number);
+
+    if(accumulated_floor_number > 100)
+        return fail;
+    else if(TOP_FLOOR == current_floor_number)
+        return top;
+    else if(BOTTOM_FLOOR == current_floor_number)
+        return bottom;
+    else if(current_floor_number < target_floor_number)
+        return up;
+    else if(current_floor_number == target_floor_number)
+        return halt;
+    else
+        return quit;
+}
+
+int event_goingDown(void)
+{
+    current_floor_number -= 1;
+    accumulated_floor_number += 1;
+    printf("Going down! Floor number is %d\n", current_floor_number);
+
+    if(accumulated_floor_number > 100)
+        return fail;
+    else if(TOP_FLOOR == current_floor_number)
+        return top;
+    else if(BOTTOM_FLOOR == current_floor_number)
+        return bottom;
+    else if(current_floor_number > target_floor_number)
+        return down;
+    else if(current_floor_number == target_floor_number)
+        return halt;
+    else
+        return quit;
+}
+
+int event_atTop(void)
+{
+    printf("At top! current_floor_number= %d\n", current_floor_number);
+    printf("Input target floor number in [1, 9] (which floor you want to go?): \n");
+    scanf("%d", &target_floor_number);
+
+    if(current_floor_number > target_floor_number)
+        return down;
+    else if(current_floor_number == target_floor_number)
+        return halt;
+    else
+        return fail;
+}
+
+int event_atBottom(void)
+{
+    printf("At Bottom! current_floor_number= %d\n", current_floor_number);
+    printf("Input target floor number in [1, 9] (which floor you want to go?): \n");
+    scanf("%d", &target_floor_number);
+
+    if(current_floor_number < target_floor_number)
+        return up;
+    else if(current_floor_number == target_floor_number)
+        return halt;
+    else
+        return fail;
+}
+
+int event_malfunction(void)
+{
+    printf("Elevator needs maintanence!\n");
+    printf("accumulated_floor_number = %d\n", accumulated_floor_number);
+    return quit;
+}
+
+int event_end(void)
+{
+    printf("Exit!");
+    return 0;
+}
+
+int event_unexpected(void)
+{
+    printf("Debug.");
+    return quit;
+}
+
+int (* event[])(void) = { event_idle, event_goingUp, event_goingDown, event_atTop, event_atBottom, event_malfunction, event_unexpected, event_end };
+   
+int lookup_transitions[][7] = 
+{ 
+    //                return codes:
+    //                up           down         halt       top         bottom         fail          quit
+    [_idle]        = {_goingUp,    _goingDown,  _idle,     _unexpected, _unexpected,  _malfunction,  _end},
+    [_goingUp]     = {_goingUp,    _unexpected, _idle,     _AtTop,      _AtBottom,    _malfunction,  _end},
+    [_goingDown]   = {_unexpected, _goingDown,  _idle,     _AtTop,      _AtBottom,    _malfunction,  _end},
+    [_AtTop]       = {_unexpected, _goingDown,  _AtTop,    _unexpected, _unexpected,  _malfunction,  _end},
+    [_AtBottom]    = {_goingUp,    _goingDown,  _AtBottom, _unexpected, _unexpected,  _malfunction,  _end},
+    [_malfunction] = {_end, _end, _end, _end, _end, _end, _end},
+    [_unexpected]  = {_end, _end, _end, _end, _end, _end, _end}
+    /* transitions from end state aren't needed */
+};
+
+int main()
+{
+    state_codes cur_state = _idle;
+    ret_codes rc;
+    int (* state_func)(void);
+    printf("Start the elevator!\n");
+
+    for (;;) 
+    {
+        state_func = event[cur_state]; // 从列表中找到对应状态的执行函数
+        rc = state_func();             // 函数执行
+        if (_end == cur_state)
+            break;
+        cur_state = lookup_transitions[cur_state][rc];
+    }
+    printf("END.");
+    return 0;
+}
+
---
+++ 99_功能/01_有限状态机FSM/01-电梯楼层/readme.md
@@ -0,0 +1,12 @@
+# 有限状态机 FSM 电梯示例
+
+## 语言
+    c语言
+
+## FSM示意图
+![FSM](./%E6%9C%89%E9%99%90%E7%8A%B6%E6%80%81%E6%9C%BA-%E7%94%B5%E6%A2%AFdemo.png)
+
+## 参考
+[b站](https://www.bilibili.com/video/BV1vV411C7uk?spm_id_from=333.337.search-card.all.click&vd_source=6b48595092f05a0fc1d129f83872951f)
+
+[github](https://github.com/horychen/FiniteStateMachineO1Demo)
